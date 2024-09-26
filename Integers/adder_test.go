4b256ef3f39528c2abaeb0b88e1d6d6d93de74c3
Keegan <keegansullivan15@gmail.com>
2024-09-26 17:37:29 -0500

    adds comment

--- Integers/adder_test.go
+++ Integers/adder_test.go
@@ -14,6 +14,9 @@ func TestAdder(t *testing.T) {
 	}
 }
 
+// Functions prepended with "Example" in the test file create testable examples of our functions
+// This is important to include so you have real examples of code usage that will break if things are changes
+// the // Output: line is a special line that will be sure this function will run with go test
 func ExampleAdd() {
 	sum := Add(1, 5)
 	fmt.Println(sum)
