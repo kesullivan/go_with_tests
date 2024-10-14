3f1091e2d83051f65432e5944fa0a3b992971cf7
keegan <keeg@Breannas-MacBook-Pro.local>
2024-10-13 20:38:02 -0500

    arrays in progress commit

---
+++ arrays/sum.go
@@ -0,0 +1,19 @@
+package main
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
